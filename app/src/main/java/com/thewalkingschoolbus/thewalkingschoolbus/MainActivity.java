474475d55a9c994e4b8747210d5ff01b9a26df2b
Fred <xuetianw@sfu.ca>
2018-02-24 08:08:34 -0800

    added base code for database

--- app/src/main/java/com/thewalkingschoolbus/thewalkingschoolbus/MainActivity.java
+++ app/src/main/java/com/thewalkingschoolbus/thewalkingschoolbus/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * MainActivity
@@ -11,17 +12,49 @@ import android.view.View;
  */
 public class MainActivity extends AppCompatActivity {
 
+    DBAdapter myDb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        openDB();
+
         setupLoginButton();
+        setupRegisterButton();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        closeDB();
+    }
+
+
+    private void openDB() {
+        myDb = new DBAdapter(this);
+        myDb.open();
+    }
+
+    private void closeDB() {
+        myDb.close();
+    }
+
+    private void setupRegisterButton() {
+        Button registerButton = (Button) findViewById(R.id.registerid);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     // Temporary - delete after proper login is written.
     private void setupLoginButton() {
-        findViewById(R.id.loginButtonid).setOnClickListener(new View.OnClickListener() {
+        Button loginButton = (Button) findViewById(R.id.loginButtonid);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = MonitoringActivity.makeIntent(MainActivity.this);
