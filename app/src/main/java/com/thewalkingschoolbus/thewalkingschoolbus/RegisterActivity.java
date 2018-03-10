895e22ec9a4806143bf50b7049889bd42524c035
Fred <xuetianw@sfu.ca>
2018-03-10 14:24:58 -0800

    made register and login work

--- app/src/main/java/com/thewalkingschoolbus/thewalkingschoolbus/RegisterActivity.java
+++ app/src/main/java/com/thewalkingschoolbus/thewalkingschoolbus/RegisterActivity.java
@@ -3,13 +3,79 @@ package com.thewalkingschoolbus.thewalkingschoolbus;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.thewalkingschoolbus.thewalkingschoolbus.Interface.OnTaskComplete;
+import com.thewalkingschoolbus.thewalkingschoolbus.Models.User;
+import com.thewalkingschoolbus.thewalkingschoolbus.api_binding.GetUserAsyncTask;
+
+import static com.thewalkingschoolbus.thewalkingschoolbus.MainActivity.*;
+import static com.thewalkingschoolbus.thewalkingschoolbus.api_binding.GetUserAsyncTask.functionType.*;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    EditText nameET;
+    EditText emailET;
+    EditText passwordET;
+    String loginName;
+    String registerPassword;
+    String registerEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        setupTextviews();
+        setupRegisterButton();
+    }
+
+    private void setupRegisterButton() {
+        Button registerBtn = (Button)findViewById(R.id.register2id);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginName = nameET.getText().toString();
+                registerEmail = emailET.getText().toString();
+                registerPassword = passwordET.getText().toString();
+
+                if(loginName.isEmpty() || registerEmail.isEmpty()|| registerPassword.isEmpty()){
+                    Toast.makeText(getApplicationContext(),MainActivity.FIELD_NOT_EMPTY_MESSAGE, Toast.LENGTH_SHORT)
+                            .show();
+                }
+
+                User user = new User();
+                user.setEmail(registerEmail);
+                user.setName(loginName);
+
+                new GetUserAsyncTask(CREATE_USER, user,null, null, registerPassword, new OnTaskComplete() {
+                    @Override
+                    public void onSuccess(Object result) {
+                        if(result != null){
+                            Toast.makeText(getApplicationContext(),REGISTER_SUCCESSFULLY_MESSAGE, Toast.LENGTH_SHORT)
+                                    .show();
+                            //Intent intent = MonitoringActivity.makeIntent(MainActivity.this);
+                            //startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(),"register failed", Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    }
+                    @Override
+                    public void onFailure(Exception e) {
+                        Toast.makeText(getApplicationContext(), "ERROR: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }).execute();
+
+            }
+        });
+    }
+
+    private void setupTextviews() {
+        nameET = (EditText)findViewById(R.id.registernameid);
+        emailET = (EditText)findViewById(R.id.registeremailid);
+        passwordET = (EditText) findViewById(R.id.registerpasswordid);
     }
 
     public static Intent makeIntent(MainActivity mainActivity) {
