b72e624d15b5e5a9fad931a8b08eadf8675d29b5
Vaanyi Igiri <vigiri@sfu.ca>
2018-03-19 18:56:46 -0700

    Implements profile mock-up using CardView.
    4 commits in 1. Encountered branching issue, had to stash-fix-unstash.

---
+++ app/src/main/java/com/thewalkingschoolbus/thewalkingschoolbus/UserProfileActivity.java
@@ -0,0 +1,10 @@
+package com.thewalkingschoolbus.thewalkingschoolbus;
+
+import android.app.Activity;
+
+/**
+ * Created by Vaanyi Igiri on 2018-03-19.
+ */
+
+public class UserProfileActivity extends Activity {
+}
