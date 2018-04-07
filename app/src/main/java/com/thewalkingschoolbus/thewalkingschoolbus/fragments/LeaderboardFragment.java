918524091f538ce655bb9ac306c1b245d2bfcac7
Fred <xuetianw@sfu.ca>
2018-04-06 19:46:18 -0700

    implement imagelistView for leader board

--- app/src/main/java/com/thewalkingschoolbus/thewalkingschoolbus/fragments/LeaderboardFragment.java
+++ app/src/main/java/com/thewalkingschoolbus/thewalkingschoolbus/fragments/LeaderboardFragment.java
@@ -1,13 +1,13 @@
 package com.thewalkingschoolbus.thewalkingschoolbus.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,6 +28,8 @@ public class LeaderboardFragment extends android.app.Fragment {
     ArrayList <User> userList = new ArrayList<>();
     User []users;
     public static ArrayList<String> arrayList;
+    TextView winnerName;
+    TextView winnderPoints;
 
     @Nullable
     @Override
@@ -62,8 +64,7 @@ public class LeaderboardFragment extends android.app.Fragment {
                             + " Name: "+ userList.get(i).getName());
                 }
 
-
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.leaderboard_entry, arrayList);
+                ArrayAdapter<String> adapter = new MyListadapter();
                 ListView list = view.findViewById(R.id.listViewLeaderboard);
                 list.setAdapter(adapter);
 
@@ -73,6 +74,51 @@ public class LeaderboardFragment extends android.app.Fragment {
                 Toast.makeText(getActivity(), "ERROR: " + e.getMessage(), Toast.LENGTH_LONG).show();
             }
         }).execute();
+    }
+
+    private class MyListadapter extends ArrayAdapter<String> {
+        public MyListadapter(){
+            super(getActivity(), R.layout.item_view, arrayList);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            //return super.getView(position, convertView, parent);
+            View itemView = convertView;
+            if ( itemView == null) {
+                itemView = getActivity().getLayoutInflater().inflate(R.layout.item_view, parent, false);
+            }
+
+            //find the gold to work with
+
+            String string = arrayList.get(position);
+            int iconid;
+            if(position == 0){
+                iconid = R.drawable.gold_medal;
+            } else if ( position == 1) {
+                iconid = R.drawable.silver_medal;
+            } else if ( position == 2 ) {
+                iconid = R.drawable.bronze_medal;
+            } else {
+                iconid = R.drawable.plainmedal;
+            }
+            ImageView imageView = (ImageView)itemView.findViewById(R.id.item_iconlid);
+            imageView.setImageResource(iconid);
+
+            winnerName = (TextView) itemView.findViewById(R.id.nameofgoldmedalwiner);
+            winnderPoints = (TextView) itemView.findViewById(R.id.totalpointesid);
+
+            winnerName.setText("Name: " + userList.get(position).getName());
+
+            winnderPoints.setText("Total Points: " + userList.get(position).getTotalPointsEarned());
+
+            return itemView;
+
+        }
+
+
+
     }
 
 }
